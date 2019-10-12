5a6631bd68aecab5abf90a5c9a4dc52305ffb7ef
chuxindalong <414203864@qq.com>
2019-10-12 20:17:11 +0800

    netarticle

---
+++ src/main/java/com/ichuxin/NetarticleApplication.java
@@ -0,0 +1,13 @@
+package com.ichuxin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetarticleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetarticleApplication.class, args);
+    }
+
+}
