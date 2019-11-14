128279924e5863d439623d65574f54716c0bac6b
chuxindalong <414203864@qq.com>
2019-11-14 15:22:47 +0800

    springboot war tomcat start successful

--- src/main/java/com/ichuxin/NetarticleApplication.java
+++ src/main/java/com/ichuxin/NetarticleApplication.java
@@ -2,9 +2,16 @@ package com.ichuxin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NetarticleApplication {
+public class NetarticleApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(NetarticleApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(NetarticleApplication.class, args);
